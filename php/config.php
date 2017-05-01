647774758f8ab70cf0f3cb76bedae368a3278a7e
htk25 <hunterkong@dhcp-gs-2396.eduroam.cornell.edu>
2017-05-01 17:02:54 -0400

    Added config for database

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php 
+// ** MySQL connection settings ** //
+
+//Keep this as localhost for the course server
+define('DB_HOST', "localhost");
+
+
+define('DB_USER', "fp_lkr");    
+ 
+
+define('DB_PASSWORD', "5p^?L=-dLeKRpSWM"); 
+
+
+define('DB_NAME', 'info230_SP17_fp_lkr');    
+?>
