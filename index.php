9585940ef78d6eed8da54740746ea38cc06eb0ba
Hao Rong <hr335@cornell.edu>
2017-05-01 16:36:53 -0400

    Main Change

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- Theme Reference: www.w3schools.com -->
-    <title>Bootstrap Theme Company Page</title>
+    <title>Cornell Swing Dance</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -14,31 +13,11 @@
     <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+<body id="myPage">
+    <?php require_once 'php/nav.php'; echo $nav?>
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-                <a class="navbar-brand" href="#myPage">Home</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#about">ABOUT</a></li>
-                    <li><a href="#events">EVENTS</a></li>
-                    <li><a href="#gallery">GALLERY</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
-                    <li><a href="#admin">ADMIN LOGIN</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <header class="clearfix" style="height: 70vh; background: url(images/theme_pic.png) no-repeat center center; background-size: cover; margin-bottom: 0px;">
+    <header class="clearfix" style="height: 70vh; background: url(images/theme_pic.png) 
+    no-repeat center center; background-size: cover; margin-bottom: 0px;">
     </header>
 
     <div class="jumbotron text-center">
@@ -67,15 +46,15 @@
     <div class="container-fluid bg-grey">
         <div class="row text-center">
             <h2>FAQ</h2>
-            <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-                <!-- Indicators -->
+            <div id="myCarousel" class="carousel text-center" data-ride="carousel">
+                
                 <ol class="carousel-indicators">
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                     <li data-target="#myCarousel" data-slide-to="1"></li>
                     <li data-target="#myCarousel" data-slide-to="2"></li>
                 </ol>
 
-                <!-- Wrapper for slides -->
+                
                 <div class="carousel-inner" role="listbox">
                     <div class="item active">
                         <h4>Q: "How can I join this amazing club?"<br><br><span>A: Submit an application form to us today!</span></h4>
@@ -88,7 +67,7 @@
                     </div>
                 </div>
 
-                <!-- Left and right controls -->
+               
                 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
@@ -101,12 +80,12 @@
         </div>
     </div>
 
-    <!-- Container (Services Section) -->
+
     <div id="events" class="container-fluid text-center">
         <h2>EVENTS</h2>
         <h4>What we offer</h4>
         <br>
-        <div class="row slideanim">
+        <div class="row">
             <div class="col-sm-4">
                 <span class="glyphicon glyphicon-off logo-small"></span>
                 <h4>POWER</h4>
@@ -124,7 +103,7 @@
             </div>
         </div>
         <br><br>
-        <div class="row slideanim">
+        <div class="row">
             <div class="col-sm-4">
                 <span class="glyphicon glyphicon-leaf logo-small"></span>
                 <h4>GREEN</h4>
@@ -143,10 +122,10 @@
         </div>
     </div>
 
-    <!-- Container (Portfolio Section) -->
+
     <div id="gallery" class="container-fluid text-center bg-grey">
         <h2>GALLERY</h2><br>
-        <div class="row text-center slideanim">
+        <div class="row text-center">
             <div class="col-sm-4">
                 <div class="thumbnail">
                     <img src="images/Example_pic1.jpg" width="400" height="300">
@@ -172,34 +151,10 @@
 
     </div>
 
-    <!-- Container (Contact Section) -->
-    <div id="contact" class="container-fluid">
-        <h2 class="text-center">CONTACT</h2>
-        <div class="row">
-            <div class="col-sm-5">
-                <p>Contact us and we'll get back to you within 24 hours.</p>
-                <p><span class="glyphicon glyphicon-map-marker"></span> Chicago, US</p>
-                <p><span class="glyphicon glyphicon-phone"></span> +00 1515151515</p>
-                <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
-            </div>
-            <div class="col-sm-7 slideanim">
-                <div class="row">
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-                    </div>
-                </div>
-                <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
-                <div class="row">
-                    <div class="col-sm-12 form-group">
-                        <button class="btn btn-default pull-right" type="submit">Send</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <footer id="footer" class="container-fluid text-center">
+        <?php require_once 'php/footer.php'; echo $footer?>
+    </footer>
+
 
 
     <!-- Add Google Maps
@@ -227,50 +182,6 @@
 To use this code on your website, get a free API key from Google.
 Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
 -->
-
-    <footer id="admin" class="container-fluid text-center">
-        <a href="#myPage" title="To Top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </a>
-
-    </footer>
-
-    <script>
-        $(document).ready(function() {
-            // Add smooth scrolling to all links in navbar + footer link
-            $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-                // Make sure this.hash has a value before overriding default behavior
-                if (this.hash !== "") {
-                    // Prevent default anchor click behavior
-                    event.preventDefault();
-
-                    // Store hash
-                    var hash = this.hash;
-
-                    // Using jQuery's animate() method to add smooth page scroll
-                    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 900, function() {
-
-                        // Add hash (#) to URL when done scrolling (default click behavior)
-                        window.location.hash = hash;
-                    });
-                } // End if
-            });
-
-            $(window).scroll(function() {
-                $(".slideanim").each(function() {
-                    var pos = $(this).offset().top;
-
-                    var winTop = $(window).scrollTop();
-                    if (pos < winTop + 600) {
-                        $(this).addClass("slide");
-                    }
-                });
-            });
-        })
-    </script>
 
 </body>
 
---
+++ php/nav.php
@@ -0,0 +1,41 @@
+<?php
+$nav = '
+		<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+            <a class="navbar-brand" href="index.php">Home</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">ABOUT<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">E-board members</a></li>
+                        <li><a href="#">History</a></li>
+                        <li><a href="#">Code of Etiquette</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">EVENT<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">Calendar</a></li>
+                        <li><a href="#">Class</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="gallery.php">GALLERY</a></li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">CONTACT<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">Suggestion</a></li>
+                        <li><a href="#">Contact Info</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#admin">ADMIN LOGIN</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>'
+?>
