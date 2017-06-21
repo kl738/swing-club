7714bd1f2714a0e3dcb9a90b9b51a6fdee901ed9
Kevin Lin <kl738@cornell.edu>
2017-06-21 11:24:21 -0400

    added irene

--- php/config.php
+++ php/config.php
@@ -3,15 +3,12 @@
 
 //Keep this as localhost for the course server and for local server with MAMP
 //using cuswingcom.domaincommysql.com for domain on cuswing.com
-define('DB_HOST', "cuswingcom.domaincommysql.com");
-//define('DB_HOST', "localhost");
-
+//define('DB_HOST', "cuswingcom.domaincommysql.com");
+define('DB_HOST', "localhost");
 
 define('DB_USER', "fp_lkr");    
  
-
 define('DB_PASSWORD', "5p^?L=-dLeKRpSWM"); 
 
-
-define('DB_NAME', 'info230_sp17_fp_lkr');    
+define('DB_NAME', 'info230_SP17_fp_lkr');    
 ?>
